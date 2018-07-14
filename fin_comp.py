76a85cf68e8ea9fdcedd02e160fcfa0148be5d76
Sean Montague <stmontague1@gmail.com>
2018-07-13 22:35:22 -0500

    Initial Commit

---
+++ fin_comp.py
@@ -0,0 +1,6 @@
+from app import app
+
+# The commands below start the web server with the application:
+# Enter the virtual env with venv/Scripts/activate
+# To run, use the command $env:FLASK_APP = "microblog.py" => flask run
+# env:FLASK_APP tells flask how to import the web application
